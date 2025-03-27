c728adf0bcd2bae4e3bf74296a62bde4bf6225a4
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2025-03-27 14:49:35 +0100

    début SAE

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// initialisaion
+
+let latitude = 0;
+let longitude = 0;
+
+
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        var map = L.map('map').setView([latitude, longitude], 18);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    }, function (erreur) {
+        console.log(erreur)
+
+        var map = L.map('map').setView([latitude, longitude], 18);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    })
+
+
+    if (latitude != 0 && longitude != 0) {
+        console.log(latitude)
+    }
+}
+else {
+    console.log("Géolocalisation non supportée");
+    var map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 13,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
+
