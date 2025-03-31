52d5c8feb65d44ea3dced2cf57ec8e6ca139ccac
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2025-03-31 09:47:10 +0200

    aniamtiosn particules

--- js/script.js
+++ js/script.js
@@ -60,113 +60,113 @@ let tab = [
     {
         x: 20,	// Position en X.
         y: 800,	// Position en Y.
-        vx: -1.14,	// Vitesse sur l’axe X.
-        vy: -1.14,	// Vitesse sur l’axe Y.
+        vx: -0.14,	// Vitesse sur l’axe X.
+        vy: -0.14,	// Vitesse sur l’axe Y.
         taille: 5
     },
     {
         x: 30,	// Position en X.
         y: 700,	// Position en Y.
-        vx: -1.8,	// Vitesse sur l’axe X.
-        vy: -1.8,	// Vitesse sur l’axe Y.
+        vx: -0.8,	// Vitesse sur l’axe X.
+        vy: -0.5,	// Vitesse sur l’axe Y.
         taille: 4
     },
     {
         x: 60,	// Position en X.
         y: 659,	// Position en Y.
-        vx: -1.2,	// Vitesse sur l’axe X.
-        vy: -1.2,	// Vitesse sur l’axe Y.
+        vx: -0.6,	// Vitesse sur l’axe X.
+        vy: -0.4,	// Vitesse sur l’axe Y.
         taille: 11
     },
     {
         x: 10,	// Position en X.
         y: 560,	// Position en Y.
-        vx: -2,	// Vitesse sur l’axe X.
-        vy: -2,	// Vitesse sur l’axe Y.
+        vx: -0.4,	// Vitesse sur l’axe X.
+        vy: -0.3,	// Vitesse sur l’axe Y.
         taille: 7
     },
     {
         x: 150,	// Position en X.
         y: 780,	// Position en Y.
-        vx: -4,	// Vitesse sur l’axe X.
-        vy: -4,	// Vitesse sur l’axe Y.
+        vx: -0.3,	// Vitesse sur l’axe X.
+        vy: -0.4,	// Vitesse sur l’axe Y.
         taille: 6
     },
     {
         x: 80,	// Position en X.
         y: 800,	// Position en Y.
-        vx: -1.5,	// Vitesse sur l’axe X.
-        vy: -1.5,	// Vitesse sur l’axe Y.
+        vx: -0.3,	// Vitesse sur l’axe X.
+        vy: -0.6,	// Vitesse sur l’axe Y.
         taille: 8
     },
     {
         x: 160,	// Position en X.
         y: 850,	// Position en Y.
-        vx: -2,	// Vitesse sur l’axe X.
-        vy: -1,	// Vitesse sur l’axe Y.
+        vx: -0.2,	// Vitesse sur l’axe X.
+        vy: -0.4,	// Vitesse sur l’axe Y.
         taille: 8
     },
     {
         x: 140,	// Position en X.
         y: 760,	// Position en Y.
-        vx: -3,	// Vitesse sur l’axe X.
-        vy: -3,	// Vitesse sur l’axe Y.
+        vx: -0.5,	// Vitesse sur l’axe X.
+        vy: -0.2,	// Vitesse sur l’axe Y.
         taille: 4
     },
     {
         x: 250,	// Position en X.
         y: 870,	// Position en Y.
-        vx: -3,	// Vitesse sur l’axe X.
-        vy: -1,	// Vitesse sur l’axe Y.
+        vx: -0.5,	// Vitesse sur l’axe X.
+        vy: -0.4,	// Vitesse sur l’axe Y.
         taille: 5
     },
     {
         x: 45,	// Position en X.
-        y: 78,	// Position en Y.
-        vx: -1,	// Vitesse sur l’axe X.
-        vy: -1,	// Vitesse sur l’axe Y.
+        y: 780,	// Position en Y.
+        vx: -0.2,	// Vitesse sur l’axe X.
+        vy: -0.4,	// Vitesse sur l’axe Y.
         taille: 6
     },
     {
-        x: 45,	// Position en X.
-        y: 78,	// Position en Y.
-        vx: -2,	// Vitesse sur l’axe X.
-        vy: -1,	// Vitesse sur l’axe Y.
+        x: 150,	// Position en X.
+        y: 780,	// Position en Y.
+        vx: -0.4,	// Vitesse sur l’axe X.
+        vy: -0.5,	// Vitesse sur l’axe Y.
         taille: 9
     },
     {
-        x: 45,	// Position en X.
-        y: 78,	// Position en Y.
-        vx: -5,	// Vitesse sur l’axe X.
-        vy: -1,	// Vitesse sur l’axe Y.
+        x: 65,	// Position en X.
+        y: 842,	// Position en Y.
+        vx: -0.1,	// Vitesse sur l’axe X.
+        vy: -0.2,	// Vitesse sur l’axe Y.
         taille: 6
     },
     {
-        x: 45,	// Position en X.
-        y: 78,	// Position en Y.
-        vx: -1,	// Vitesse sur l’axe X.
-        vy: -0.9,	// Vitesse sur l’axe Y.
+        x: 20,	// Position en X.
+        y: 760,	// Position en Y.
+        vx: -0.5,	// Vitesse sur l’axe X.
+        vy: -0.3,	// Vitesse sur l’axe Y.
         taille: 6
     },
     {
-        x: 215,	// Position en X.
-        y: 32,	// Position en Y.
+        x: 56,	// Position en X.
+        y: 740,	// Position en Y.
         vx: -0.2,	// Vitesse sur l’axe X.
         vy: -0.2,	// Vitesse sur l’axe Y.
         taille: 7
     },
     {
-        x: 321,	// Position en X.
+        x: 69,	// Position en X.
         y: 862,	// Position en Y.
-        vx: -1,	// Vitesse sur l’axe X.
-        vy: -0.6,	// Vitesse sur l’axe Y.
+        vx: -0.5,	// Vitesse sur l’axe X.
+        vy: -0.3,	// Vitesse sur l’axe Y.
         taille: 3
     },
     {
         x: 45,	// Position en X.
-        y: 890,	// Position en Y.
-        vx: -2,	// Vitesse sur l’axe X.
-        vy: -2,	// Vitesse sur l’axe Y.
+        y: 840,	// Position en Y.
+        vx: -0.5,	// Vitesse sur l’axe X.
+        vy: -0.2,	// Vitesse sur l’axe Y.
         taille: 9
     },
 ]
@@ -179,17 +179,32 @@ function initialisation() {
     canvaparti.height = window.innerHeight
 }
 
-function boucle(){
-    ctx.clearRect(0, 0, window.innerWidth, window.innerWidth)
-    
+function boucle() {
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
     tab.forEach(e => {
+        e.x += e.vx
+        e.y += e.vy
+        if(e.x <= 0){
+        
+        }
+        // Création d'un gradient radial centré sur la particule
+
+        let gradient = ctx.createRadialGradient(
+            e.x + e.taille / 2, e.y + e.taille / 2, 0,  // Centre du gradient
+            e.x + e.taille / 2, e.y + e.taille / 2, e.taille / 2 // Rayon extérieur
+        );
+
+        // Ajout des couleurs (effet lumineux)
+        gradient.addColorStop(0, "yellow"); // Blanc au centre
+        gradient.addColorStop(0.7, "orange"); // Bleu intermédiaire
+
+        // Application du gradient
+        ctx.fillStyle = gradient;
         ctx.fillRect(e.x, e.y, e.taille, e.taille)
     })
 
     window.requestAnimationFrame(boucle)
 }
-
-
 
 initialisation();
 boucle();
