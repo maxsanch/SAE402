569b3ed8eee3c86b7eafce3d3b95661679b8d827
Alexandre Spitzer <alexandre.spitzer@uha.fr>
2025-04-02 19:04:27 +0200

    suite du jeux

--- jeu_script/jeu2.js
+++ jeu_script/jeu2.js
@@ -1,34 +1,37 @@
-var canvas_decor = document.getElementById("maisons");
-var canvas_decor = document.getElementById("route");
-var canvas_obstacles = document.getElementById("obstacles");
-var canvas_perso = document.getElementById("perso");
-        var ctx = canvas.getContext("2d");
+let canvas_maisons = document.getElementById("maisons");
+let canvas_route = document.getElementById("route");
+let canvas_obstacles = document.getElementById("obstacles");
+let canvas_perso = document.getElementById("perso");
+let maisons = canvas_maisons.getContext("2d");
+let route = canvas_route.getContext("2d");
+let obstacles = canvas_obstacles.getContext("2d");
+let perso = canvas_perso.getContext("2d");
 
-        // définit la taille de l'écran
-        ecrW = window.innerWidth;
-        ecrH = window.innerHeight;
+// définit la taille de l'écran
+ecrW = window.innerWidth;
+ecrH = window.innerHeight;
 
-        // définit la taille du canvas
-        canvas.width = ecrW;
-        canvas.height = ecrH;
+// définit la taille du canvas
+canvas_perso.width = ecrW;
+canvas_perso.height = ecrH;
 
-        // initialisation des variables
-        var hauteur = ecrW;
-        var largeur = ecrH;
+// initialisation des variables
+var largeur = ecrW;
+var hauteur = ecrH;
 
+var line = 0;
+var fact = 1;
 
-        var line = 0;
-        var fact = 1;
+function AfficherFond() {
+    // Utilisation du contexte 'perso'
+    perso.clearRect(0, 0, largeur, hauteur);
 
-        function AfficherFond() {
-            ctx.clearRect(0, 0, hauteur, largeur);
+    // rectangle de couleur
+    perso.fillStyle = "red";
+    perso.fillRect(0, 0, largeur, hauteur);
 
-            // rectangle jaune
-            ctx.fillStyle = "blue"
-            ctx.fillRect(0, 0, hauteur, largeur);
+    window.requestAnimationFrame(AfficherFond);
+}
 
-            window.requestAnimationFrame(AfficherFond);
-        }
-
-        // Appel de la fonction pour l'afficher sur le site
-        AfficherFond();
+// Appel de la fonction pour l'afficher sur le site
+AfficherFond();
