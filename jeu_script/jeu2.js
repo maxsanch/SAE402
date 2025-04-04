4a76a7bea4a2ed02884df6fb23e85d11a1702a54
Alexandre Spitzer <alexandre.spitzer@uha.fr>
2025-04-04 09:53:03 +0200

    Update jeu2.js

--- jeu_script/jeu2.js
+++ jeu_script/jeu2.js
@@ -14,24 +14,43 @@ ecrH = window.innerHeight;
 // définit la taille du canvas
 canvas_perso.width = ecrW;
 canvas_perso.height = ecrH;
+canvas_obstacles.width = ecrW;
+canvas_obstacles.height = ecrH;
+canvas_maisons.width = ecrW;
+canvas_maisons.height = ecrH;
+canvas_route.width = ecrW;
+canvas_route.height = ecrH;
 
 // initialisation des variables
 var largeur = ecrW;
 var hauteur = ecrH;
 
-var line = 0;
-var fact = 1;
+// initialisation des variables pour le personnage
+var persoX = 0; // position X du personnage à l'horizontale
+var persoY = 0; // position Y du personnage à la verticale
 
-function AfficherFond() {
-    // Utilisation du contexte 'perso'
+// initialisation des variables de la route
+var roadL = 0;
+var roadM = 0;
+var roadR = 0;
+var spdRoute = 0;
+
+// initialisation des variables de vitesse pour les obstacles / maisons
+var spdObstc = 0;
+var spdMais = 0;
+
+// persoY = ecrW * 0.2;
+persoY = ecrW * 0.4;
+// persoY = ecrW * 0.2;
+
+function AfficherPerso() {
     perso.clearRect(0, 0, largeur, hauteur);
 
-    // rectangle de couleur
-    perso.fillStyle = "red";
-    perso.fillRect(0, 0, largeur, hauteur); 
+    perso.fillStyle = "black";
+    perso.fillRect(persoY, persoX, 50, 50);
 
-    window.requestAnimationFrame(AfficherFond);
+    window.requestAnimationFrame(AfficherPerso);
 }
 
 // Appel de la fonction pour l'afficher sur le site
-AfficherFond();
+AfficherPerso();
