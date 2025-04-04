76bbe78b4fa73cae468063d9500d39c26ca5a02c
Alexandre Spitzer <alexandre.spitzer@uha.fr>
2025-04-04 11:45:22 +0200

    ajout de la route et le perso se place correctement

--- jeu_script/jeu2.js
+++ jeu_script/jeu2.js
@@ -11,7 +11,7 @@ let perso = canvas_perso.getContext("2d");
 ecrW = window.innerWidth;
 ecrH = window.innerHeight;
 
-// définit la taille du canvas
+// définit la taille des canvas
 canvas_perso.width = ecrW;
 canvas_perso.height = ecrH;
 canvas_obstacles.width = ecrW;
@@ -21,36 +21,45 @@ canvas_maisons.height = ecrH;
 canvas_route.width = ecrW;
 canvas_route.height = ecrH;
 
-// initialisation des variables
+// initialisation des variables globales
 var largeur = ecrW;
 var hauteur = ecrH;
+var gravité = 1;
 
 // initialisation des variables pour le personnage
-var persoX = 0; // position X du personnage à l'horizontale
-var persoY = 0; // position Y du personnage à la verticale
+var persoX = 200; // position X du personnage à l'horizontale
+var persoY = 200; // position Y du personnage à la verticale
+var tailleX = 50; // taille du personnage à l'horizontale
+var tailleY = 50; // taille du personnage à la verticale
 
 // initialisation des variables de la route
-var roadL = 0;
-var roadM = 0;
-var roadR = 0;
+var epaisseurRoute = 50;
+var roadL = (largeur - 3 * epaisseurRoute) / 4;
+var roadM = roadL + epaisseurRoute + ((largeur - 3 * epaisseurRoute) / 4);
+var roadR = roadM + epaisseurRoute + (largeur - 3 * epaisseurRoute) / 4;
 var spdRoute = 0;
 
 // initialisation des variables de vitesse pour les obstacles / maisons
 var spdObstc = 0;
 var spdMais = 0;
 
-// persoY = ecrW * 0.2;
-persoY = ecrW * 0.4;
-// persoY = ecrW * 0.2;
-
-function AfficherPerso() {
+function Afficher() {
     perso.clearRect(0, 0, largeur, hauteur);
 
     perso.fillStyle = "black";
+    perso.fillRect(persoY - (tailleY / 2), persoX - (tailleX / 2), tailleY, tailleX);
+    perso.fillStyle = "red";
     perso.fillRect(persoY, persoX, 50, 50);
 
-    window.requestAnimationFrame(AfficherPerso);
+    // affichage de la route
+    route.clearRect(0, 0, largeur, hauteur);
+    route.fillStyle = "gray";
+    route.fillRect(roadL, 0, epaisseurRoute, hauteur);
+    route.fillRect(roadM, 0, epaisseurRoute, hauteur);
+    route.fillRect(roadR, 0, epaisseurRoute, hauteur);
+
+    window.requestAnimationFrame(Afficher);
 }
 
 // Appel de la fonction pour l'afficher sur le site
-AfficherPerso();
+Afficher();
