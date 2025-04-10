ad50356aa2265d553ab68e4c3d8fa1a80b9aab94
Alexandre Spitzer <alexandre.spitzer@uha.fr>
2025-04-10 17:26:07 +0200

    les obstacles peuvent êtres uniques

--- jeu_script/jeu2.js
+++ jeu_script/jeu2.js
@@ -24,7 +24,7 @@ canvas_route.height = ecrH;
 // initialisation des variables globales
 let largeur = ecrW;
 let hauteur = ecrH;
-let gravité = 1;
+let gravité = 0.05;
 
 // initialisation des variables de la route
 let epaisseurRoute = 50;
@@ -45,16 +45,20 @@ let tailleY = 50; // taille du personnage à la verticale
 // initialisation des variables pour les obstacles
 let obstacleY = 0; // position Y de l'obstacle à la verticale
 let obstacleX = road[CurrentRoad]; // position X de l'obstacle à l'horizontale
-
+let obstacleExist = true; // variable pour vérifier si l'obstacle existe ou non
 
 // initialisation des variables de vitesse pour les obstacles & maisons / ennemis
 let spdObstc = 6;
 let spdEnmy = 1;
 let ennemiX = road[0]; // position X de l'ennemi à l'horizontale
+let ennemiExist = true; // variable pour vérifier si l'ennemi existe ou non
 
 // Initialisation d'une variable pour le score
 let score = 0;
 let viePlayer = 3; // Nombre de vies du joueur
+
+// Initialisation de variable que j'ai plus envie de définir
+let decideExist = 0; // Variable qui décide de qui va disparaître de notre réalité
 
 //////////////////////////////////////////////////////////
 
@@ -120,38 +124,53 @@ function Afficher() {
     route.clearRect(0, 0, largeur, hauteur);
     maisons.clearRect(0, 0, largeur, hauteur);
     // joueur
-    perso.fillStyle = "black"; 
+    perso.fillStyle = "black";
     perso.fillRect(persoY - (tailleY / 2), persoX - (tailleX / 2), tailleY, tailleX);
-    // obstacle
-    obstacles.fillStyle = "red";
-    obstacles.fillRect(obstacleX - (tailleX / 2), obstacleY, tailleY, tailleX);
+    // obstacle 1
+    if (obstacleExist == true) {
+        obstacles.fillStyle = "red";
+        obstacles.fillRect(obstacleX - (tailleX / 2), obstacleY, tailleY, tailleX);
+    }
     // obstacle 2
-    obstacles.fillStyle = "orange";
-    obstacles.fillRect(ennemiX - (tailleX / 2), obstacleY, tailleY, tailleX);
+    if (ennemiExist == true) {
+        obstacles.fillStyle = "orange";
+        obstacles.fillRect(ennemiX - (tailleX / 2), obstacleY, tailleY, tailleX);
+    }
 
     obstacleY += spdObstc; // Déplacement de l'obstacle vers le bas
     if (obstacleY > hauteur) {
         obstacleY = 0; // Réinitialise la position de l'obstacle
         obstacleX = road[Math.floor(Math.random() * 3)]; // Change la position de l'obstacle aléatoirement
         ennemiX = road[Math.floor(Math.random() * 3)]; // Change la position de l'obstacle aléatoirement
-        // if ennemiX == obstacleX { // Si l'ennemi et l'obstacle sont sur la même voie, on change la position de l'ennemi
-        // }
-        if (collision === true){
+        if (collision === true) {
             viePlayer--; // Diminue le nombre de vies
-            console.log("Score actuelle", score,"vie restante" , viePlayer);
+            console.log("Score actuelle", score, "vie restante", viePlayer);
         }
-        else{
+        else {
             score++; // Augmente le score
-            console.log("Score actuelle", score,"vie restante" , viePlayer);
+            console.log("Score actuelle", score, "vie restante", viePlayer);
         }
+        spdObstc += gravité; // Augmente la vitesse de l'obstacle
+        spdEnmy += gravité; // Augmente la vitesse de l'ennemi
+        console.log("Vitesse de l'obstacle", spdObstc, "Vitesse de l'ennemi", spdEnmy);
+        decideExist = Math.floor(Math.random() * 2); // 0 ou 1 pour décider de qui va disparaître
         collision = false; // Réinitialise la variable de collision
+        obstacleExist = true; // Réinitialise l'existence de l'obstacle
+        ennemiExist = true; // Réinitialise l'existence de l'ennemi
         // score++; // Augmente le score
         // console.log(score);
     }
-
+    if (ennemiX == obstacleX) { // Si l'ennemi et l'obstacle sont sur la même voie, on change la position de l'ennemi
+        if (decideExist == 0) {
+            obstacleExist = false; // L'obstacle n'existe plus
+        }
+        else {
+            ennemiExist = false; // L'ennemi n'existe plus
+        }
+    }
 
     detectCollision(); // Vérifie les collisions
-    
+
 
     // affichage de la route
     route.clearRect(0, 0, largeur, hauteur);
@@ -160,13 +179,13 @@ function Afficher() {
     route.fillRect(roadM, 0, epaisseurRoute, hauteur);
     route.fillRect(roadR, 0, epaisseurRoute, hauteur);
 
-    if (viePlayer <= 0) {
+    if (viePlayer <= 0) { // Si le joueur n'a plus de vies
         window.cancelAnimationFrame(Afficher); // Arrête l'animation si le joueur n'a plus de vies
         clearScreen(); // retire les canvass en les passant en display none
-        if (score >= 30){
+        if (score >= 30) {
             console.log("Vous avez gagné, Votre score est de " + score + " !");
         }
-        else{
+        else {
             console.log("Vous avez perdu, Votre score est de " + score + " ! Vous devez faire un score supérieur à 30 pour gagner.");
         }
     }
