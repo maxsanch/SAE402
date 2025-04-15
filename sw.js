b00491a7dc7db0cb105fa7447cc0f9a93af6bc0f
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2025-04-15 15:44:39 +0200

    mise a jour test

--- sw.js
+++ sw.js
@@ -4,69 +4,111 @@
 
 var version = 'v1:0:0';
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
 	self.skipWaiting();
 	event.waitUntil(
-	caches.open(version + 'fundamentals')
-      .then(function(cache) {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'manifest.json'
-        ]);
-      })
+		caches.open(version + 'fundamentals')
+			.then(function (cache) {
+				return cache.addAll([
+					'/',
+					'index.html',
+					'manifest.json',
+					'styles/style.css',
+					'js/dialogues.json',
+					'js/script.js',
+					'jeu_style/jeu1.css',
+					'jeu_style/jeu2.css',
+					'jeu_style/jeu3.css',
+					'jeu_script/jeu1.js',
+					'jeu_script/jeu2.js',
+					'jeu_script/jeu3.js',
+					'jeu/jeu1.html',
+					'jeu/jeu2.html',
+					'jeu/jeu3.html',
+					'font/Daydream.ttf',
+					'audio/jeu3/Donkey Kong Country Music SNES - DK Island Swing',
+					'audio/jeu3/tremblements.mp3',
+					'img/arrow_back.svg',
+					'img/back-tonneau.png',
+					'img/fond-jeu-3.jpg',
+					'img/fond-jeu3-debut.jpg',
+					'img/fond-sans-ombres.png',
+					'img/fondbar.jpg',
+					'img/fondbarrel.jpg',
+					'img/fondintro.jpg',
+					'img/fondJeu3.jpg',
+					'img/fondrun.jpg',
+					'img/fondscene1.jpg',
+					'img/front-sans-ombres.png',
+					'img/front-tonneau.png',
+					'img/plaine.png',
+					'img/sky.jpg',
+					'img/personnages/Barman.png',
+					'img/personnages/Lambda.png',
+					'img/personnages/Tanner.png',
+					'img/personnages/Us.png',
+					'img/iconeApp/icon-72x72.png',
+					'img/iconeApp/icon-96x96.png',
+					'img/iconeApp/icon-128x128.png',
+					'img/iconeApp/icon-144x144.png',
+					'img/iconeApp/icon-152x152.png',
+					'img/iconeApp/icon-192x192.png',
+					'img/iconeApp/icon-384x384.png',
+					'img/iconeApp/icon-512x512.png'
+				]);
+			})
 	);
 });
 
-self.addEventListener("fetch", function(event) {
-  if (event.request.url.indexOf('http') === 0 && event.request.method == 'GET') {
-	  event.respondWith(
-		caches
-		  .match(event.request)
-		  .then(function(cached) {
-			var networked = fetch(event.request)
-			  .then(fetchedFromNetwork, unableToResolve)
-			  .catch(unableToResolve);
-			return cached || networked;
+self.addEventListener("fetch", function (event) {
+	if (event.request.url.indexOf('http') === 0 && event.request.method == 'GET') {
+		event.respondWith(
+			caches
+				.match(event.request)
+				.then(function (cached) {
+					var networked = fetch(event.request)
+						.then(fetchedFromNetwork, unableToResolve)
+						.catch(unableToResolve);
+					return cached || networked;
 
-			function fetchedFromNetwork(response) {
-			  var cacheCopy = response.clone();
-			  caches.open(version + 'pages')
-					.then(function add(cache) {
-						cache.put(event.request, cacheCopy);
-					});
-			  return response;
-			}
+					function fetchedFromNetwork(response) {
+						var cacheCopy = response.clone();
+						caches.open(version + 'pages')
+							.then(function add(cache) {
+								cache.put(event.request, cacheCopy);
+							});
+						return response;
+					}
 
-			function unableToResolve () {
-			 
-			  return new Response("<h1>Cette ressource n'est pas disponible hors ligne</h1>", {
-				status: 503,
-				statusText: 'Service Unavailable',
-				headers: new Headers({
-				  'Content-Type': 'text/html'
+					function unableToResolve() {
+
+						return new Response("<h1>Cette ressource n'est pas disponible hors ligne</h1>", {
+							status: 503,
+							statusText: 'Service Unavailable',
+							headers: new Headers({
+								'Content-Type': 'text/html'
+							})
+						});
+					}
 				})
-			  });
-			}
-		  })
-	  );
-  }
+		);
+	}
 });
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches
-      .keys()
-      .then(function (keys) {
-        return Promise.all(
-          keys
-            .filter(function (key) {
-              return !key.startsWith(version);
-            })
-            .map(function (key) {
-              return caches.delete(key);
-            })
-        );
-      })
-  );
+self.addEventListener("activate", function (event) {
+	event.waitUntil(
+		caches
+			.keys()
+			.then(function (keys) {
+				return Promise.all(
+					keys
+						.filter(function (key) {
+							return !key.startsWith(version);
+						})
+						.map(function (key) {
+							return caches.delete(key);
+						})
+				);
+			})
+	);
 });
